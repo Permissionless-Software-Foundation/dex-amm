d80b2e7d6d0d93649d21df56077ac6af6b67b296
Chris Troutner <chris.troutner@gmail.com>
2023-05-11 13:29:09 -0700

    fix(timer controller): preventing race condition between timer intervals

--- src/controllers/timer-controllers.js
+++ src/controllers/timer-controllers.js
@@ -24,6 +24,11 @@ class TimerControllers {
     // Bind 'this' object to all subfunctions.
     this.checkOrders = this.checkOrders.bind(this)
 
+    // State
+    this.state = {
+      checkOrdersHasFinished: true
+    }
+
     this.startTimers()
   }
 
@@ -39,15 +44,31 @@ class TimerControllers {
 
   async checkOrders () {
     try {
+      // Exit if the previous call has not finshed.
+      if (!this.state.checkOrdersHasFinished) return
+
+      // Disable the timer interval while this function runs.
+      this.state.checkOrdersHasFinished = false
+      clearInterval(this.checkOrderHandle)
+
       console.log('checkOrders() timer controller fired')
       // console.log('this.useCases: ', this.useCases)
 
       const hash = await this.useCases.order.checkOrders()
 
+      // Renable this timer interval
+      this.state.checkOrdersHasFinished = true
+      this.checkOrderHandle = setInterval(this.checkOrders, 60000 * 10)
+
       return hash
     } catch (err) {
       // Do not throw an error. This is a top-level function.
-      console.log('Error in timer-controllers.js/gcOrders(): ', err)
+      console.log('Error in timer-controllers.js/checkOrders(): ', err)
+
+      // Renable this timer interval
+      this.state.checkOrdersHasFinished = true
+      this.checkOrderHandle = setInterval(this.checkOrders, 60000 * 10)
+
       return false
     }
   }
